339163ec8ae2b15a13e7812bb8133abce0778abb
Toniton <akinjiolatoni@gmail.com>
2018-07-10 03:34:47 +0100

    added production url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'https://paytron.com.ng',
-  API_PORT: '8081',
+  API_URL: 'https://toniton-webtrekk.herokuapp.com',
+  API_PORT: '3000',
   PREFIX: 'api'
 };
